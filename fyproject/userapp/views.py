63e0943429666b5a00aab352ce256ae436f6a68b
hananeoba <hananeouldbabaali@gmail.com>
2024-02-22 17:42:18 +0100

    user_without_token

--- fyproject/userapp/views.py
+++ fyproject/userapp/views.py
@@ -5,34 +5,35 @@ from django.contrib.auth.hashers import make_password
 from .models import User
 from .serializer import UserSerializer
 from django.utils import timezone
+from rest_framework.authtoken.models import Token
+
 
 class CreatedFieldsMixin:
     def perform_create(self, serializer):
-        instance = serializer.save()
-        instance.set_password(instance.password)
-        instance.save()
         created_fields = {
-            'created_by': self.request.user if self.request.user.is_authenticated else None,
-            'updated_by': None,
-            'created_at': timezone.now(),
-            'updated_at': None,
-            'password': make_password(serializer.validated_data['password'])
+            "created_by": (
+                self.request.user if self.request.user.is_authenticated else None
+            ),
+            "updated_by": None,
+            "created_at": timezone.now(),
+            "updated_at": None,
+            "password": make_password(serializer.validated_data["password"]),
         }
         serializer.save(**created_fields)
-        
+
     def perform_update(self, serializer):
-        instance = serializer.save()
-        instance.set_password(instance.password)
-        instance.save()
         updated_fields = {
-            
-            'updated_by': self.request.user if self.request.user.is_authenticated else None,
-            'updated_at': timezone.now(),
-            'password': make_password(serializer.validated_data['password'])
+            "updated_by": (
+                self.request.user if self.request.user.is_authenticated else None
+            ),
+            "updated_at": timezone.now(),
+            "password": make_password(serializer.validated_data["password"]),
         }
         serializer.save(**updated_fields)
 
- # Import the missing module
+
+# Import the missing module
+
 
 class UserListView(CreatedFieldsMixin, generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -40,8 +41,12 @@ class UserListView(CreatedFieldsMixin, generics.ListCreateAPIView):
     """authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
     """
+
+
 class UserDetailView(CreatedFieldsMixin, generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    lookup_field = 'username'
+    lookup_field = "username"
+
+
 # Create your views here.
